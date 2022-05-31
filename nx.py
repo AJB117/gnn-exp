824a2c811da59903eb320e591978b5a3f976bc80
patricksoga <psoga@nd.edu>
2022-05-30 23:54:48 -0700

    attempt

--- nx.py
+++ nx.py
@@ -2,6 +2,7 @@ import networkx as nx
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 from random import randint
 
 def get_empty_edge(A):
@@ -9,33 +10,39 @@ def get_empty_edge(A):
     x = np.random.choice(zeros.shape[0], 1, replace=False)
     return zeros[x][0]
 
+def embed_ohe(G):
+    n = []
+    for node in G.nodes:
+        ohe = np.zeros(len(G.nodes), dtype=np.float32)
+        ohe[node] = 1
+        n.append((node, {
+            'x': ohe
+        }))
+    H = nx.Graph()
+    H.add_nodes_from(n)
+    H.add_edges_from(G.edges)
+    return H
+
 trees = []
 non_trees = []
-k = 0
+
 for _ in range(300):
     G = nx.random_tree(20)
+    G = embed_ohe(G)
     trees.append(G)
 
     H = nx.random_tree(20)
     nodes = H.nodes
-    num_edges = randint(1, 5)
+    num_edges = randint(10, 15)
     A = nx.adjacency_matrix(H)
     edges = [get_empty_edge(A) for _ in range(num_edges)]
     for (u, v) in edges:
         H.add_edge(u, v)
-    if nx.is_tree(H):
-        k += 1
+    H = embed_ohe(H)
     non_trees.append(H)
+
+pickle.dump([torch.tensor([[1, 0]], dtype=torch.float)]*len(trees), open('./trees_labels.pkl', 'wb'))
+pickle.dump([torch.tensor([[0, 1]], dtype=torch.float)]*len(non_trees), open('./non_trees_labels.pkl', 'wb'))
 
 pickle.dump(trees, open('./trees.pkl', 'wb'))
 pickle.dump(non_trees, open('./non_trees.pkl', 'wb'))
-
-trees = pickle.load(open('./trees.pkl', 'rb'))
-non_trees = pickle.load(open('./non_trees.pkl', 'rb'))
-tree = trees[0]
-non_tree = non_trees[0]
-
-fig, axes = plt.subplots(2)
-nx.draw(tree, with_labels=True, ax=axes[0])
-nx.draw(non_tree, with_labels=True, ax=axes[1])
-plt.show()
--- pyg-gc.py
+++ pyg-gc.py
@@ -1,16 +1,18 @@
 import pickle
+from sklearn.utils import shuffle
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 from torch_geometric.utils.convert import from_networkx
-from torch_geometric.nn import GCNConv
+import torch_geometric.nn as pyg_nn
 from argparse import ArgumentParser
 
-# implement readout, export to file with other models
 class GCN(torch.nn.Module):
     def __init__(self, num_features, num_hidden, num_classes) -> None:
         super().__init__()
-        self.conv1 = GCNConv(num_features, num_hidden)
-        self.conv2 = GCNConv(num_hidden, num_classes)
+        self.conv1 = pyg_nn.GCNConv(num_features, num_hidden)
+        self.conv2 = pyg_nn.GCNConv(num_hidden, num_hidden)
+        self.ll = nn.Linear(num_hidden, num_classes)
 
     def forward(self, data):
         x, edge_idx = data.x, data.edge_index
@@ -18,8 +20,9 @@ class GCN(torch.nn.Module):
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_idx)
-
-        return F.sigmoid(x)
+        x = pyg_nn.global_mean_pool(x, batch=None, size=None)
+        x = self.ll(x)
+        return torch.sigmoid(x)
 
 def split(data, split=(0.7, 0.15, 0.15)):
     n = len(data)
@@ -30,25 +33,57 @@ def split(data, split=(0.7, 0.15, 0.15)):
 
     return train, val, test
 
+def to_device(data, device):
+    new = [(d[0].to(device), d[1].to(device)) for d in data]
+    return new
+
 def main(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     trees = pickle.load(open('./trees.pkl', 'rb'))
     non_trees = pickle.load(open('./non_trees.pkl', 'rb'))
 
-    tree_data = [from_networkx(t) for t in trees]
-    non_tree_data = [from_networkx(n) for n in non_trees]
+    trees_labels = pickle.load(open('./trees_labels.pkl', 'rb'))
+    non_trees_labels = pickle.load(open('./non_trees_labels.pkl', 'rb'))
+
+    tree_data = [(from_networkx(t), trees_labels[i]) for i, t in enumerate(trees)]
+    non_tree_data = [(from_networkx(n), non_trees_labels[i]) for i, n in enumerate(non_trees)]
 
     train_t, val_t, test_t = split(tree_data)
     train_n, val_n, test_n = split(non_tree_data)
 
-    train = train_t + train_n
-    val = val_t + val_n
-    test = test_t + test_n
+    train = to_device(train_t + train_n, device)
+    val = to_device(val_t + val_n, device)
+    test = to_device(test_t + test_n, device)
 
     num_hidden = args.hidden
+    model = GCN(train[0][0].x.shape[0], num_hidden, 2).to(device)
 
-    model = GCN(num_hidden, num_hidden, 2)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    loss_fn = nn.CrossEntropyLoss()
+
+    for e, epoch in enumerate(range(200)):
+        model.train()
+        train = shuffle(train)
+        train_loss = 0.
+        for i, d in enumerate(train):
+            optimizer.zero_grad()
+            out = model(d[0])
+            loss = loss_fn(out, d[1])
+            train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        val_correct = 0
+        for i, d in enumerate(val):
+            pred = model(d[0])
+            if pred.argmax().item() == torch.where(d[1][0] > 0)[0].item():
+                val_correct += 1
+        
+        print(f'Epoch {e} train_loss: {train_loss}, val_acc: {val_correct/len(val)}')
 
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("--hidden", type=int)
+    p.add_argument("--batch_size", type=int)
     main(p.parse_args())
