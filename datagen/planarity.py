0807b2c4aa62b0859357a9eb6cfda6baac1fc68f
patricksoga <psoga@nd.edu>
2022-06-23 16:52:20 -0700

    save files

--- datagen/planarity.py
+++ datagen/planarity.py
@@ -2,6 +2,7 @@ import sys
 import networkx as nx
 import matplotlib.pyplot as plt
 from argparse import ArgumentParser
+from pickle import dump
 
 def generate_planarity_er(num_graphs, n):
     planar_graphs = []
@@ -22,6 +23,9 @@ def generate_planarity_er(num_graphs, n):
 def main(args):
     planar_graphs, non_planar_graphs = generate_planarity_er(args.num_graphs, args.n)
 
+    dump(planar_graphs, open(args.filenames[0], 'wb'))
+    dump(non_planar_graphs, open(args.filenames[1], 'wb'))
+
     if not args.verify: return
 
     for i, (planar, nonplanar) in enumerate(zip(planar_graphs, non_planar_graphs)):
@@ -38,4 +42,5 @@ if __name__ == '__main__':
     p.add_argument("--n", help="number of nodes", default=20)
     p.add_argument("--num_graphs", help="number of graphs to generate (planar and non-planar)", default=1000)
     p.add_argument("--verify", help="view the graphs after generation", action="store_true")
+    p.add_argument("--filenames", default=["../data/planar.pkl", "../data/non_planar.pkl"])
     main(p.parse_args())
