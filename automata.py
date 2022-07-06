e1b7c524a59f2701750bd950a282dc4767b1cd5d
patricksoga <psoga@nd.edu>
2022-07-05 18:46:33 -0700

    pos enc visualization

--- automata.py
+++ automata.py
@@ -9,19 +9,31 @@ def main(args):
     n_nodes = args.n_nodes
     n_states = args.n_states
 
-    # initial_weights = torch.Tensor([[0]]*n_states)
-    # initial_weights[0][0] = 1.
-    # initial_weights = torch.cat((initial_weights, torch.zeros(n_states, n_nodes-1)), dim=1)
+    initial_weights = torch.Tensor([[1]]*n_states)
+    # initial_weights = torch.rand((n_states, 1))
+    initial_weights = torch.cat((initial_weights, torch.zeros(n_states, n_nodes-1)), dim=1)
 
-    initial_weights = torch.rand(n_states, n_nodes)
+    # initial_weights = torch.zeros(n_states, n_nodes)
+    # initial_weights = torch.rand(n_states, n_nodes)
     # initial_weights = torch.ones(n_states, n_nodes)
 
-    path_graph = nx.path_graph(n_nodes)
+    graph = nx.DiGraph()
+    edges = []
+    for i in range(n_nodes-1):
+        edges.append((i, i+1))
+    graph.add_edges_from(edges)
+
+    # graph = nx.path_graph(n_nodes)
+    # graph = nx.cycle_graph(n_nodes)
+    # graph = nx.random_powerlaw_tree(n_nodes, tries=100)
+    # graph = nx.erdos_renyi_graph(n_nodes, 0.1)
 
     # mu = torch.zeros((n_states, n_states))
-    # mu = torch.rand((n_states, n_states))
-    mu = torch.ones((n_states, n_states))
-    A = torch.from_numpy(nx.adjacency_matrix(path_graph).A).type(torch.float)
+    mu = torch.rand((n_states, n_states))
+    # mu = torch.ones((n_states, n_states))
+    # mu = torch.eye(n_states)
+    # mu = torch.ones(n_states, n_states).fill_diagonal_(0)
+    A = torch.from_numpy(nx.adjacency_matrix(graph).A).type(torch.float)
 
     # flatten in column-major order
     vec_init = initial_weights.transpose(1, 0).flatten()
@@ -38,7 +50,7 @@ def main(args):
     weights = torch.linalg.solve(B, c)
 
     # undo vectorization
-    weights = torch.stack(weights.split(n_states), dim=1)
+    weights = torch.stack(weights.split(n_states), dim=1).type(torch.double)
     print(f"weights for {n_nodes} nodes with {n_states} states: \n", weights)
 
     plt.figure("encoding")
@@ -48,7 +60,8 @@ def main(args):
     ax.invert_yaxis()
 
     plt.figure("similarity")
-    ax = sb.heatmap(weights.T @ weights, cmap='hot')
+    similarity = weights.T @ weights
+    ax = sb.heatmap(similarity, cmap='hot')
     ax.set_xlabel("Enc at node i")
     ax.set_ylabel("Enc at node i")
     ax.invert_yaxis()
@@ -57,6 +70,6 @@ def main(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--n_nodes", help="number of nodes", default=50, type=int)
-    parser.add_argument("--n_states", help="number of states", default=20, type=int)
+    parser.add_argument("--n_nodes", help="number of nodes", default=10, type=int)
+    parser.add_argument("--n_states", help="number of states", default=3, type=int)
     main(parser.parse_args())
