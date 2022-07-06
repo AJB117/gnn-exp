28a1b19eada6661b2e54a7314515949c04497076
patricksoga <psoga@nd.edu>
2022-07-06 10:54:55 -0700

    new gen options

--- datagen/dataset_generator.py
+++ datagen/dataset_generator.py
@@ -1,17 +1,24 @@
 import sys
-from turtle import position
 import numpy as np
 import networkx as nx
 import torch
 import dgl
 from pickle import dump, load
 from abc import abstractmethod
+from random import randint
+import matplotlib.pylab as plt 
 
 class DatasetGenerator:
-    def __init__(self, dataset_name: str) -> None:
-        self.dataset_name = dataset_name
-        self.positive = []
-        self.negative = []
+    def __init__(self, prefix: str) -> None:
+        self.prefix = prefix
+        try:
+            self.positive = load(open(f'../data/{prefix}', 'rb'))
+        except FileNotFoundError:
+            self.positive = []
+        try:
+            self.negative = load(open(f'../data/non_{prefix}', 'rb'))
+        except FileNotFoundError:
+            self.negative = []
 
     @staticmethod
     def add_one_hots(G, num_features):
@@ -38,12 +45,13 @@ class DatasetGenerator:
     def generate(self, num_graphs, n):
         raise NotImplementedError
 
-    def save_to_dgl(self):
-        positive = load(open('../data/' + self.dataset_name, 'rb'))
-        negative = load(open('../data/non_' + self.dataset_name, 'rb'))
+    @abstractmethod
+    def check_proportions(self):
+        raise NotImplementedError
 
-        negative = map(self.add_one_hots_dgl, map(dgl.from_networkx, negative))
-        positive = map(self.add_one_hots_dgl, map(dgl.from_networkx, positive))
+    def save_to_dgl(self):
+        negative = map(self.add_one_hots_dgl, map(dgl.from_networkx, self.negative))
+        positive = map(self.add_one_hots_dgl, map(dgl.from_networkx, self.positive))
         all_data = []
 
         for p, n in zip(positive, negative):
@@ -51,71 +59,108 @@ class DatasetGenerator:
             all_data.append((n, torch.tensor([0])))
         
         # dump(all_data, open(f"../data/{self.dataset_name}.pkl", "wb"))
-        torch.save(all_data, f"../data/{self.dataset_name}.pkl",)
+        torch.save(all_data, f"../data/{self.prefix}.pkl",)
 
     def save(self):
-        dump([torch.tensor([[0, 1]], dtype=torch.float)]*len(self.positive), open("../data/" + self.dataset_name + '.labels', 'wb'))
-        dump([torch.tensor([[1, 0]], dtype=torch.float)]*len(self.negative), open("../data/" + "non_" + self.dataset_name + '.labels', 'wb'))
+        dump([torch.tensor([[0, 1]], dtype=torch.float)]*len(self.positive), open("../data/" + self.prefix + '.labels', 'wb'))
+        dump([torch.tensor([[1, 0]], dtype=torch.float)]*len(self.negative), open("../data/" + "non_" + self.prefix + '.labels', 'wb'))
 
-        dump(self.positive, open("../data/" + self.dataset_name, 'wb'))
-        dump(self.negative, open("../data/" + "non_" + self.dataset_name, 'wb'))
+        dump(self.positive, open("../data/" + self.prefix, 'wb'))
+        dump(self.negative, open("../data/" + "non_" + self.prefix, 'wb'))
 
 class K3ColorableDatasetGenerator(DatasetGenerator):
-    def generate(self, num_graphs, n):
+    def generate(self, num_graphs, lower_n, upper_n):
         k3_colorable_graphs = []
         non_k3_colorable_graphs = []
-        tries = 0
+
         while len(k3_colorable_graphs) != num_graphs or len(non_k3_colorable_graphs) != num_graphs:
-            sys.stdout.write(f"\r try: {tries}")
+            sys.stdout.write(f"\r k3colorable - non_k3colorable: {len(k3_colorable_graphs)} - {len(non_k3_colorable_graphs)}")
 
-            candidate = nx.erdos_renyi_graph(n, 0.2)
-            coloring = nx.coloring.greedy_color(candidate)
-            is_k3_colorable = len(set(coloring.values())) <= 3
+            positive_found = False
+            negative_found = False
+            n = randint(lower_n, upper_n)
 
-            candidate = self.add_one_hots(candidate, n)
+            while not negative_found or not positive_found:
+                candidate = nx.erdos_renyi_graph(n, 0.2)
+                coloring = nx.coloring.greedy_color(candidate)
+                is_k3_colorable = len(set(coloring.values())) <= 3
+                candidate = self.add_one_hots(candidate, n)
 
-            if is_k3_colorable:
-                k3_colorable_graphs.append(candidate)
+                if is_k3_colorable and len(k3_colorable_graphs) != num_graphs and not positive_found:
+                    k3_colorable_graphs.append(candidate)
+                    positive_found = True
+                if not is_k3_colorable and len(non_k3_colorable_graphs) != num_graphs and not negative_found:
+                    non_k3_colorable_graphs.append(candidate)
+                    negative_found = True
 
-            if not is_k3_colorable and len(non_k3_colorable_graphs) != num_graphs:
-                non_k3_colorable_graphs.append(candidate)
+            # candidate = nx.erdos_renyi_graph(n, 0.2)
+            # coloring = nx.coloring.greedy_color(candidate)
+            # is_k3_colorable = len(set(coloring.values())) <= 3
 
-            tries += 1
+            # candidate = self.add_one_hots(candidate, n)
+
+            # if is_k3_colorable:
+            #     k3_colorable_graphs.append(candidate)
+
+            # if not is_k3_colorable and len(non_k3_colorable_graphs) != num_graphs:
+            #     non_k3_colorable_graphs.append(candidate)
 
         self.positive = k3_colorable_graphs
         self.negative = non_k3_colorable_graphs
 
         return k3_colorable_graphs, non_k3_colorable_graphs
 
+    def check_proportions(self):
+        positive_sizes = [len(x.nodes) for x in self.positive]
+        negative_sizes = [len(x.nodes) for x in self.negative]
+        plt.figure("3-colorable graph node number dist")
+        plt.hist(positive_sizes)
+        plt.figure("Non-3-colorable graph node number dist")
+        plt.hist(negative_sizes)
+        plt.show()
+
 class PlanarDatsetGenerator(DatasetGenerator):
-    def generate(self, num_graphs, n):
+    def generate(self, num_graphs, lower_n, upper_n):
         planar_graphs = []
         non_planar_graphs = []
-        tries = 0
+
         while len(planar_graphs) != num_graphs or len(non_planar_graphs) != num_graphs:
-            sys.stdout.write(f"\r try: {tries}")
+            sys.stdout.write(f"\r planar - non_planar: {len(planar_graphs)} - {len(non_planar_graphs)}")
 
-            candidate = nx.erdos_renyi_graph(n, 0.15)
-            is_planar = nx.is_planar(candidate)
-            candidate = self.add_one_hots(candidate, n)
+            positive_found = False
+            negative_found = False
+            n = randint(lower_n, upper_n)
 
-            if is_planar:
-                planar_graphs.append(candidate)
-            if not is_planar and len(non_planar_graphs) != num_graphs:
-                non_planar_graphs.append(candidate)
-            tries += 1
+            while not negative_found or not positive_found:
+                candidate = nx.erdos_renyi_graph(n, 0.1)
+                is_planar = nx.is_planar(candidate)
+                candidate = self.add_one_hots(candidate, n)
+
+                if is_planar and len(planar_graphs) != num_graphs and not positive_found:
+                    planar_graphs.append(candidate)
+                    positive_found = True
+                if not is_planar and len(non_planar_graphs) != num_graphs and not negative_found:
+                    non_planar_graphs.append(candidate)
+                    negative_found = True
 
         self.positive = planar_graphs
         self.negative = non_planar_graphs
 
         return planar_graphs, non_planar_graphs
 
-    def check_planarity_proportions(self):
-        positive = load(open('../data/planarity', 'rb'))
+    def check_proportions(self):
         count = 0
-        for graph in positive:
+        for graph in self.positive:
             n = len(graph.nodes)
             e = len(graph.edges)
             if (3*n - 6) < e:
                 count += 1
-        print("Ratio of positive graphs that violate e <= 3v-6: ", count/len(positive))
+        
+        positive_sizes = [len(x.nodes) for x in self.positive]
+        negative_sizes = [len(x.nodes) for x in self.negative]
+        plt.figure("Planar graph node number dist")
+        plt.hist(positive_sizes)
+        plt.figure("Non-planar graph node number dist")
+        plt.hist(negative_sizes)
+        print("Ratio of positive graphs that violate e <= 3v-6: ", count/len(self.positive))
+        plt.show()
--- datagen/gen.py
+++ datagen/gen.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import torch
 from argparse import ArgumentParser
 from dataset_generator import K3ColorableDatasetGenerator, PlanarDatsetGenerator
 
@@ -8,9 +9,19 @@ def main(args):
         gen = K3ColorableDatasetGenerator(args.prefix)
     if args.prefix == "planarity":
         gen = PlanarDatsetGenerator(args.prefix)
+    
+    if args.view:
+        gen.check_proportions()
+        return
+    
+    if args.dglView:
+        graphs = torch.load(open(f"../data/{args.prefix}.pkl", 'rb'))
+        print(graphs[0])
+        return
 
-    positive, negative = gen.generate(args.num_graphs, args.n)
+    positive, negative = gen.generate(args.num_graphs, args.lower_n, args.upper_n)
 
+    gen.save()
     gen.save_to_dgl()
 
     if not args.verify: return
@@ -26,9 +37,11 @@ def main(args):
 
 if __name__ == '__main__':
     p = ArgumentParser()
-    p.add_argument("--n", help="number of nodes", default=20)
-    p.add_argument("--num_graphs", help="number of graphs to generate (planar and non-planar)", default=1000)
+    p.add_argument("--lower_n", help="lower bound for number of nodes to include in graph", default=20, type=int)
+    p.add_argument("--upper_n", help="upper bound for number of nodes to include in graph", default=20, type=int)
+    p.add_argument("--num_graphs", help="number of graphs to generate (planar and non-planar)", default=1000, type=int)
     p.add_argument("--verify", help="view the graphs after generation", action="store_true")
-    p.add_argument("--prefix", default="k3colorable")
+    p.add_argument("--prefix", default="k3colorable", choices=["k3colorable", "planarity"])
     p.add_argument("--view", action="store_true")
+    p.add_argument("--dglView", action="store_true")
     main(p.parse_args())
